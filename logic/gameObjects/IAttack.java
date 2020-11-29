0e9d19dfd1e5773be3273a0d9cf2c5b500287b2b
Liidiacnog <72378386+Liidiacnog@users.noreply.github.com>
2020-11-29 13:45:58 +0100

    duda en iAttack puesta como TODO

--- logic/gameObjects/IAttack.java
+++ logic/gameObjects/IAttack.java
@@ -2,7 +2,7 @@ package logic.gameObjects;
 
 public interface IAttack {
 
-	void attack(int columns);
+	void attack(int columns); //TODO: what would happen if it weren't in this interface? Ok to pass it the oclumns though Vampire won't use them?
 
 	default boolean receiveSlayerAttack(int damage) {return false;}
 	
