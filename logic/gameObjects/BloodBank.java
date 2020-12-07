68e01282b92f4f1d5f5342fb6b418573162c9382
olbap tenrof <olbap tenrof@192.168.1.193>
2020-12-07 12:33:12 +0100

    BloodBank y explosion del vampiro hecho

---
+++ logic/gameObjects/BloodBank.java
@@ -0,0 +1,42 @@
+package logic.gameObjects;
+
+import logic.Game;
+
+public class BloodBank extends GameElement {
+	
+	private int cost;
+	private boolean alive;
+	private static final String representation = "B-B";
+	
+	public BloodBank(int x, int y, int cost, Game game) {
+		super(x, y, game);
+		this.cost = cost;
+		alive = true;
+	}
+	
+	public boolean receiveVampireAttack(int harm) {
+		alive = false;
+		return true;
+	}
+	
+	public String toString() {
+		return representation + "[" + cost + "]";
+	}
+	
+	@Override
+	public boolean isDead() {
+		return !alive;
+	}
+	
+	@Override
+	public int getBlBaCoins() {
+		return Math.round(cost * 0.1f);
+	}
+	
+	@Override
+	public void move() {}
+
+	@Override
+	public void attack() {}
+
+}
